795c31aec2a8c9b852557a0e4ef2e64988c13070
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-17 20:01:34 -0300

    Adicionar classe Contador

---
+++ src/main/java/br/com/desafio/desafio/modelos/Contador.java
@@ -0,0 +1,11 @@
+package br.com.desafio.desafio.modelos;
+
+public class Contador {
+
+    public void contador(int numero) {
+        for(int i = 1; i <= numero; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
