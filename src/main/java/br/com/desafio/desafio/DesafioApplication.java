0aae6c748ad59f3a2b4378ecdfb608d07021a86a
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-16 21:48:03 -0300

    Adicionar classe DesafioApplication

---
+++ src/main/java/br/com/desafio/desafio/DesafioApplication.java
@@ -0,0 +1,24 @@
+package br.com.desafio.desafio;
+
+import br.com.desafio.desafio.services.LerNumero;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("teste");
+
+		LerNumero lerNumero = new LerNumero();
+		int numeroDigitado = lerNumero.lerNumero();
+		System.out.println(numeroDigitado);
+	}
+}
